52c82ed2ef3ef62e8d33b5e9e1545014b6c61b69
longfangsong <longfangsong@icloud.com>
2025-03-12 14:56:40 +0100

    build frontend

--- src/lib.rs
+++ src/lib.rs
@@ -12,14 +12,15 @@ pub mod utils;
 
 
 #[wasm_bindgen]
-pub fn to_dots(input: &str) -> Vec<String> {
+pub fn to_dots(input: &str) -> JsValue {
     let (pltl_formula, atom_map) = PLTL::from_string(input);
     let pltl_formula = pltl_formula.normal_form();
     let context = automata::Context::new(&pltl_formula, atom_map);
     let all_sub_automatas = automata::AllSubAutomatas::new(&context);
-    let mut result = Vec::new();
-    result.extend(all_sub_automatas.guarantee_automatas.values().map(|automata| to_dot(automata)));
-    result.extend(all_sub_automatas.safety_automatas.values().map(|automata| to_dot(automata)));
-    result.extend(all_sub_automatas.stable_automatas.iter().map(|automata| to_dot(automata)));
-    result
+    let result: Vec<Vec<String>> = vec![
+        all_sub_automatas.guarantee_automatas.values().map(|automata| to_dot(automata)).collect(),
+        all_sub_automatas.safety_automatas.values().map(|automata| to_dot(automata)).collect(),
+        all_sub_automatas.stable_automatas.iter().map(|automata| to_dot(automata)).collect(),
+    ];
+    serde_wasm_bindgen::to_value(&result).unwrap()
 }
